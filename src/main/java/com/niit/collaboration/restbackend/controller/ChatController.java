64d1d2f4dbb9caecce57425c74fbd64e0bc69561
pkumar <pkumar@linux-j2zf>
2017-01-17 02:42:32 +0530

    Implementing Chat in rest-backend

---
+++ src/main/java/com/niit/collaboration/restbackend/controller/ChatController.java
@@ -0,0 +1,20 @@
+package com.niit.collaboration.restbackend.controller;
+
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.niit.collaboration.backend.model.Message;
+import com.niit.collaboration.backend.model.OutputMessage;
+
+@Controller
+public class ChatController {
+
+	@MessageMapping("/chat")
+	@SendTo("/topic/message")
+	public OutputMessage sendMessage(Message message) {
+		return new OutputMessage(message, new Date());
+	}
+}
